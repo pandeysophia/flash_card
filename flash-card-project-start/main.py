d7da68bcfa66b67db6696d141ffea853cfada266
pandeysophia <sophiapandey2017@gmail.com>
2021-01-21 12:46:14 -0700

    "FLip Feature Added"

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -5,24 +5,38 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("./data/french_words.csv")
 word_list = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_word():
-    word_choice = random.choice(word_list[random.randint(0, 100)]["French"])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(word_list)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
-    #french_word.config(text=word_list[random.randint(0, 100)]["French"])
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=back_image)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_img)
+back_image = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=front_img)
+
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-french_word = canvas.create_text(400, 263, text=word_list[random.randint(0, 100)]["French"], font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_image = PhotoImage(file="images/wrong.png")
@@ -32,5 +46,7 @@ wrong_button.grid(column=0, row=1)
 right_image = PhotoImage(file="images/right.png")
 right_button = Button(image=right_image, highlightthickness=0, relief="flat", bg=BACKGROUND_COLOR, command=next_word)
 right_button.grid(column=1, row=1)
+
+next_word()
 
 window.mainloop()
